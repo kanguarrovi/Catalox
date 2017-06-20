a32b428fe560472025fe1dfdbce3887cbd43b4fe
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2017-06-20 17:44:55 -0600

    Inheritance in AudioFormat

--- Catalogo/models.py
+++ Catalogo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-class Vinilo(models.Model):
-    id = models.AutoField(primary_key=True)
+class Formato(models.Model):
     imagen = models.ImageField(blank=True)
     artista = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
@@ -13,15 +12,12 @@ class Vinilo(models.Model):
     def __str__(self):
         return "%s. %s - %s (Precio: ¢%s / Apartado: %s / Vendido: %s)" % (self.id, self.artista, self.nombre, self.precio, self.apartado, self.vendido)
 
-class CD(models.Model):
-    id = models.AutoField(primary_key=True)
-    imagen = models.ImageField(blank=True)
-    artista = models.CharField(max_length=100)
-    nombre = models.CharField(max_length=100)
-    precio = models.IntegerField()
-    apartado = models.BooleanField(default=False)
-    vendido = models.BooleanField(default=False)
-    info = models.TextField(blank=True)
+    class Meta:
+        abstract = True
 
-    def __str__(self):
-        return "%s. %s - %s (Precio: ¢%s / Apartado: %s / Vendido: %s)" % (self.id, self.artista, self.nombre, self.precio, self.apartado, self.vendido)
+class Vinilo(Formato):
+    id = models.AutoField(primary_key=True)
+
+class CD(Formato):
+    id = models.AutoField(primary_key=True)
+    
