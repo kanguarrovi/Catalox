ee8ba333a5e22e4811b4ab84ac43be31a8db2e52
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2017-07-10 02:50:31 -0600

    Update model

--- Catalogo/models.py
+++ Catalogo/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
-class Formato(models.Model):
+class Format(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
     imagen = models.ImageField(blank=True)
     artista = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
+    formato = models.ForeignKey(Format, on_delete = models.CASCADE)
     precio = models.IntegerField()
     apartado = models.BooleanField(default=False)
     vendido = models.BooleanField(default=False)
@@ -11,13 +19,4 @@ class Formato(models.Model):
 
     def __str__(self):
         return "%s. %s - %s (Precio: ¢%s / Apartado: %s / Vendido: %s)" % (self.id, self.artista, self.nombre, self.precio, self.apartado, self.vendido)
-
-    class Meta:
-        abstract = True
-
-class Vinilo(Formato):
-    id = models.AutoField(primary_key=True)
-
-class CD(Formato):
-    id = models.AutoField(primary_key=True)
     
