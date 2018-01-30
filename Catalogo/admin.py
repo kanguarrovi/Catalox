19a440558a5ec5705c49407420a5c049996c11c4
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-01-30 00:08:38 -0600

    Born again project with Flask

--- Catalogo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Vinilo
-from .models import CD
-
-admin.site.register(Vinilo)
-admin.site.register(CD)
--- Catalogo/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def printf():
-    print ("This is nothing import at all")
