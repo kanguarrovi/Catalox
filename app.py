bc1e72ccb6a997c05a39b90cae9fd76be2e88d60
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-01-31 01:42:47 -0600

    Database up0

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import sqlite3 as sql
 from flask import Flask, render_template
+from database.connection import query_db
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	con = sql.connect("database/vinyl-catalox.db")
+	con.row_factory = sql.Row
+
+	cur = con.cursor()
+	cur.execute("select * from Vinyl")
+
+	albums = cur.fetchall()
+	return render_template('index.html',  albums = albums)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,27 @@
 import os
-from sqlalchemy import create_engine
+import sqlite3
+from flask import g
 
-def return_engine():
+"""def return_engine():
 	#return create_engine('sqlite:////home/kangu/Escritorio/restful-project/interview_question/database/salaries.db')
-	return create_engine('sqlite:///' + os.getcwd() + '/vinyl-catalox.db')
+	return create_engine('sqlite:///' + os.getcwd() + '/vinyl-catalox.db')"""
+
+DATABASE = os.getcwd() + '/vinyl-catalox.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+"""@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()"""
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
