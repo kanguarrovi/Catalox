c258213a7cc561eab5d4fb80841bb8456452958d
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-01-30 21:41:05 -0600

    Database refactor

--- database/vinyl-catalox.sql
+++ database/vinyl-catalox.sql
@@ -1,10 +1,13 @@
 CREATE TABLE Vinyl(
-	id INTEGER(11) PRIMARY KEY ASC,
-    image VARCHAR(255),
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image VARCHAR(255) NULL,
     artist VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     price INT(11) NOT NULL,
     saved BOOLEAN DEFAULT 0,
     sold BOOLEAN DEFAULT 0,
-    info TEXT
+    info TEXT DEFAULT ""
 );
+
+INSERT INTO Vinyl(artist, name, price)  
+VALUES ("Edge of Sanity","Crimson","27500");
