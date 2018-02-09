1813e45f371a3dfe08c1f4dfa30999cae1178205
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-02-09 16:58:26 -0600

    Erase archives

--- database/database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('catalox.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE Vinyl(id INTEGER PRIMARY KEY AUTOINCREMENT,image VARCHAR(255) NULL,artist VARCHAR(255) NOT NULL,name VARCHAR(255) NOT NULL,price REAL NOT NULL,saved BOOLEAN DEFAULT 0,sold BOOLEAN DEFAULT 0,info TEXT DEFAULT "");')
-print("Table created successfully")
-conn.close()
